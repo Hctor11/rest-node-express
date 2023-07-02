5c9bb8b9d9da9ed48e58b53d907772770fbd3058
hector <hectorrrivera1010@gmail.com>
2023-07-01 23:34:20 -0600

    add config.js file with the port

---
+++ src/config.js
@@ -0,0 +1,2 @@
+// Defining the route port of the application
+export const PORT = process.env.PORT || 2222;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-import express from 'express';
-
+import express from 'express'
+import { PORT } from './config';
 // setup express
 /*
 The app.use() function adds a new middleware to the app. Essentially, 
@@ -7,8 +7,12 @@ whenever a request hits your backend, Express will execute the functions
 you passed to app.use() in order.
 */
 const app = express();
+// express.json() is a built in middleware function in Express starting from v4.16.0. 
+// It parses incoming JSON requests and puts the parsed data in req.body.
 app.use(express.json())
 
-// Defining the route port of the application
-const PORT = process.env.PORT || 2222;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
 
